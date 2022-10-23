c375935fafebb32f56c9028f92c312509892875e
Miguel Fernandez <miguelfera@gmail.com>
2022-10-23 17:39:38 -0600

    A portfolio website example using ReacJS

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://i.pinimg.com/originals/8d/37/94/8d3794772e95e3fdf81d0563c67e1170.jpg",
+      link: "http://lama.dev",
+    },
+  ];
